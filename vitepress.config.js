2897d9b114da4946dea171be6e83774e572e4967
Eric Mogollon <eric@marteu.com>
2025-02-15 14:44:11 -0500

    Fix HTML asset paths and update VitePress configuration

--- vitepress.config.js
+++ vitepress.config.js
@@ -1,6 +1,9 @@
-// vitepress.config.js
-module.exports = {
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
   title: "Docs test practico",
   description: "Documentación test practico",
   base: "/docs-test-practico/",
-};
+  cleanUrls: true,
+  assetsDir: "assets",
+});
